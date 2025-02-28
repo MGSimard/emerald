beba0e23524ec20d06df4bac9bf976eedc3bd0ec
MGSimard <mgsimard.dev@gmail.com>
2025-02-28 01:44:13 -0500

    a

--- src/_components/NotFoundError.tsx
+++ src/_components/NotFoundError.tsx
@@ -15,7 +15,6 @@ export function Error() {
   return (
     <main className="not-found">
       <h1>Error</h1>
-      {/* Add error message catch? */}
       <Link to="/" className="link">
         RETURN HOME
       </Link>
