5dfb21a9b729629f2e2e16a3e5e8db1303112f77
MGSimard <mgsimard.dev@gmail.com>
2025-02-27 22:43:22 -0500

    Configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { resolve } from "node:path";
+
+const ReactCompilerConfig = {
+  target: "19",
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+    ,
+  ],
   test: {
     globals: true,
     environment: "jsdom",
   },
-
 });
