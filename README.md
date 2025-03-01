f3a31178e01bd96065f0f713fd6f650dcabf8a63
MGSimard <mgsimard.dev@gmail.com>
2025-03-01 03:02:15 -0500

    Update description, start adding faq modal

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Emerald
 
+Emerald est une application de suivi local-first, optimisée pour gérer le progrès des Avis de recherche sur Dofus.
+
 ## Task List
 
 - [ ] Finalize colors (need better light mode palette)
--- src/_components/InfoButton.tsx
+++ src/_components/InfoButton.tsx
@@ -1,11 +1,22 @@
+import { useRef } from "react";
 import { IconQuestionMark } from "@/_components/Icons";
 
 export function InfoButton() {
+  const dialogRef = useRef<HTMLDialogElement>(null);
   // Popover API?
 
   return (
-    <button type="button" className="question-mark">
-      <IconQuestionMark />
-    </button>
+    <>
+      <button type="button" className="question-mark" onClick={() => dialogRef.current?.showModal()}>
+        <IconQuestionMark />
+      </button>
+      <dialog id="faq-dialog" ref={dialogRef}>
+        <h2>Information</h2>
+        <p>
+          Emerald est une application de suivi local-first, optimisée pour gérer le progrès des Avis de recherche dans
+          Dofus.
+        </p>
+      </dialog>
+    </>
   );
 }
