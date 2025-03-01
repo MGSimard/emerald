943ecf1e869c1e4f9a6933e5fccce22eb568f65e
MGSimard <mgsimard.dev@gmail.com>
2025-03-01 04:12:20 -0500

    ea sports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import tsParser from "@typescript-eslint/parser";
 import reactCompiler from "eslint-plugin-react-compiler";
 
 export default tseslint.config(tseslint.configs.recommendedTypeChecked, tseslint.configs.stylisticTypeChecked, {
+  ignores: ["node_modules", "dist"],
   plugins: {
     "react-compiler": reactCompiler,
   },
