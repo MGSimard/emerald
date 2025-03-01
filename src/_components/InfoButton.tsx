b8e568b6552ab9463da0cdfe13e2c02867e1e20c
MGSimard <mgsimard.dev@gmail.com>
2025-03-01 03:41:00 -0500

    faq

--- src/_components/InfoButton.tsx
+++ src/_components/InfoButton.tsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { IconQuestionMark } from "@/_components/Icons";
+import { IconQuestionMark, IconClose } from "@/_components/Icons";
 
 export function InfoButton() {
   const dialogRef = useRef<HTMLDialogElement>(null);
@@ -11,12 +11,40 @@ export function InfoButton() {
         <IconQuestionMark />
       </button>
       <dialog id="faq-dialog" ref={dialogRef}>
-        <h2>Information</h2>
-        <p>
-          Emerald est une application de suivi local-first, optimisée pour gérer le progrès des Avis de recherche dans
-          Dofus.
-        </p>
+        <div className="dialog-header">
+          <h2>Information</h2>
+          <button
+            type="button"
+            className="btn square"
+            onClick={() => dialogRef.current?.close()}
+            aria-label="Fermer"
+            title="Fermer">
+            <IconClose />
+          </button>
+        </div>
+
+        {qs.map((q) => (
+          <section>
+            <h3> {q.q} </h3>
+            <p>{q.a}</p>
+          </section>
+        ))}
       </dialog>
     </>
   );
 }
+
+const qs = [
+  {
+    q: "Qu'est-ce Emerald?",
+    a: "Emerald est une application de suivi local-first, optimisée pour gérer le progrès des Avis de recherche dans Dofus.",
+  },
+  {
+    q: "Que veux dire 'local-first'?",
+    a: "L'application stocke les données de votre progrès localement, sur votre ordinateur, à l'aide du system 'IndexedDB' de votre navigateur. Votre base de donnée se synchronise périodiquement avec les informations du serveur afin de mettre a jour les données des recherchés en cas de changements.",
+  },
+  {
+    q: "Est-ce que Emerald sauvegarde mes données?",
+    a: "Non. Votre progression est stockée sur votre ordinateur uniquement.",
+  },
+];
